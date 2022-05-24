7d22022c089180e5cb7370936d1349181ed8abc1
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2022-05-24 10:41:54 -0300

    Test

--- tests/repositories/test_s3_repository.py
+++ tests/repositories/test_s3_repository.py
@@ -1,13 +1,15 @@
 import unittest
 
-from src.repositories.s3_repository import S3Client, S3Config
+from src.repositories.s3_repository import S3Client, Config
+from src.config.dotenv import load_dotenv
 
 
 class TestS3Repository(unittest.TestCase):
 
     def test_setup_bucket(self):
-        config = S3Config(source=dict(S3_ACCESS_KEY='M8HHAKCPE0ISM2LL0E9X',
-                                      S3_SECRET_KEY='dN9OjGDJUA54tDiAaBCIMxixEgf75CIcF3H60ZNn',
-                                      S3_BUCKET_NAME='dev-bucket',
-                                      S3_ENDPOINT_URL='https://teste-videos.us-east-1.linodeobjects.com/'))
+        # TODO: Remover teste 
+        config = Config(source=dict(S3_ACCESS_KEY='M8HHAKCPE0ISM2LL0E9X',
+                                    S3_SECRET_KEY='dN9OjGDJUA54tDiAaBCIMxixEgf75CIcF3H60ZNn',
+                                    S3_BUCKET_NAME='dev-bucket',
+                                    S3_ENDPOINT_URL='https://teste-videos.us-east-1.linodeobjects.com/'))
         client = S3Client(config)
